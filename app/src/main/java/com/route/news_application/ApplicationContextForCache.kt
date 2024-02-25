2a91edfa2a5b46a210a0d054a5369c7809703087
sherif Abdelrhman <sellkady038@gmail.com>
2024-02-25 14:27:37 +0200

    Finish Callback between Activity and Adapter. Handling Sources Call Based on Specific Category. Finish Design For Search View.

--- app/src/main/java/com/route/news_application/ApplicationContextForCache.kt
+++ app/src/main/java/com/route/news_application/ApplicationContextForCache.kt
@@ -1,11 +1,14 @@
 package com.route.news_application
 
 import android.app.Application
+import android.app.UiModeManager.MODE_NIGHT_NO
 import android.content.Context
+import androidx.appcompat.app.AppCompatDelegate
 
 class ApplicationContextForCache :Application() {
     override fun onCreate() {
         super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         application = this
     }
     companion object{
